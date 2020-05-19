88706c3dc0082bc1f632276c1e5ecfe6057a127e
theresamorelli <theresa.l.morelli@gmail.com>
2020-05-18 17:17:37 -0700

    Add form render

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+class BookInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "book-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const bookInput = new BookInput();
