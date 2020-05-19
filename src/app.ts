71cb62478faf37726b3df708d1ce81fcc40c237a
theresamorelli <theresa.l.morelli@gmail.com>
2020-05-18 17:43:10 -0700

    Add autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
+// Decorator
+function Autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  }
+  return adjDescriptor;
+}
+
 class BookInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  authorInputElement: HTMLInputElement;
+  priorityInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -14,7 +34,31 @@ class BookInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.authorInputElement = this.element.querySelector(
+      "#author"
+    ) as HTMLInputElement;
+    this.priorityInputElement = this.element.querySelector(
+      "#priority"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log("this", this);
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
